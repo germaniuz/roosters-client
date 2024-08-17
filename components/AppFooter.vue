a368aa19f3aab3608c4da8023a62948cef6048c5
Juliya Snegova <juliya_snegova@studio-aw.ru>
2024-08-18 01:24:50 +0300

    refactor: refactor styles

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,75 +3,77 @@
 
 <template>
   <footer class="footer">
-      <div class="footer__contacts-block">
-          <div class="footer__download-apps">
-              <div class="footer__download-app">
-                  <BaseIcon name="playmarket"/>
-                  <span class="footer__download-app-text">Скачать из</span>
-                  <span class="footer__download-app-title">Google Play</span>
+      <div class="footer__top container">
+          <div class="footer__contacts-block">
+              <div class="footer__download-apps">
+                  <div class="footer__download-app">
+                      <BaseIcon name="playmarket"/>
+                      <span class="footer__download-app-text">Скачать из</span>
+                      <span class="footer__download-app-title">Google Play</span>
+                  </div>
+                  <div class="footer__download-app">
+                      <BaseIcon name="appstore"/>
+                      <span class="footer__download-app-text">Загрузите в</span>
+                      <span class="footer__download-app-title">App Store</span>
+                  </div>
+                  <div class="footer__download-app">
+                      <BaseIcon name="huawei"/>
+                      <span class="footer__download-app-text">Откройте в</span>
+                      <span class="footer__download-app-title">AppGallery</span>
+                  </div>
               </div>
-              <div class="footer__download-app">
-                  <BaseIcon name="appstore"/>
-                  <span class="footer__download-app-text">Загрузите в</span>
-                  <span class="footer__download-app-title">App Store</span>
+              <div class="footer__contacts">
+                  <div class="footer__contact">
+                      <img src="/images/icons/phone.svg" alt="" class="footer__contact-image">
+                      <a class="footer__contact-text" href="tel:8 (8442) 33-77-77">8 (8442) 33-77-77</a>
+                      <div class="footer__contact-subtext">Бесплатный звонок</div>
+                  </div>
+                  <div class="footer__contact">
+                      <img src="/images/icons/mail.svg" alt="" class="footer__contact-image">
+                      <a class="footer__contact-text" href="mailto:roosters-pizza@yandex.ru">roosters-pizza@yandex.ru</a>
+                      <div class="footer__contact-subtext">Для отзывов и предложений</div>
+                  </div>
               </div>
-              <div class="footer__download-app">
-                  <BaseIcon name="huawei"/>
-                  <span class="footer__download-app-text">Откройте в</span>
-                  <span class="footer__download-app-title">AppGallery</span>
-              </div>
-          </div>
-          <div class="footer__contacts">
-              <div class="footer__contact">
-                  <img src="/images/icons/phone.svg" alt="" class="footer__contact-image">
-                  <a class="footer__contact-text" href="tel:8 (8442) 33-77-77">8 (8442) 33-77-77</a>
-                  <div class="footer__contact-subtext">Бесплатный звонок</div>
-              </div>
-              <div class="footer__contact">
-                  <img src="/images/icons/mail.svg" alt="" class="footer__contact-image">
-                  <a class="footer__contact-text" href="mailto:roosters-pizza@yandex.ru">roosters-pizza@yandex.ru</a>
-                  <div class="footer__contact-subtext">Для отзывов и предложений</div>
-              </div>
-          </div>
-          <div class="footer__socials">
-              <div class="footer__social">
-                  <BaseIcon name="instagram"/>
-              </div>
-              <div class="footer__social">
-                  <BaseIcon name="vk"/>
-              </div>
-          </div>
-      </div>
-      <div class="footer__nav">
-          <div class="footer__nav-item">
-              <a href="#" class="footer__nav-item-title">Рустерс</a>
-              <div class="footer__nav-subitems">
-                  <a class="footer__nav-subitem" href="#">О Нас</a>
-                  <a class="footer__nav-subitem" href="#">Новости</a>
-                  <a class="footer__nav-subitem" href="#">Вакансии</a>
-                  <a class="footer__nav-subitem" href="#">Контакты</a>
+              <div class="footer__socials">
+                  <div class="footer__social">
+                      <BaseIcon name="instagram"/>
+                  </div>
+                  <div class="footer__social">
+                      <BaseIcon name="vk"/>
+                  </div>
               </div>
           </div>
-          <div class="footer__nav-item">
-              <a href="#" class="footer__nav-item-title">Покупателям</a>
-              <div class="footer__nav-subitems">
-                  <a class="footer__nav-subitem" href="#">Отзывы</a>
-                  <a class="footer__nav-subitem" href="#">Качество</a>
-                  <a class="footer__nav-subitem" href="#">Доставка</a>
-                  <a class="footer__nav-subitem" href="#">Оплата</a>
+          <div class="footer__nav">
+              <div class="footer__nav-item">
+                  <a href="#" class="footer__nav-item-title">Рустерс</a>
+                  <div class="footer__nav-subitems">
+                      <a class="footer__nav-subitem" href="#">О Нас</a>
+                      <a class="footer__nav-subitem" href="#">Новости</a>
+                      <a class="footer__nav-subitem" href="#">Вакансии</a>
+                      <a class="footer__nav-subitem" href="#">Контакты</a>
+                  </div>
+              </div>
+              <div class="footer__nav-item">
+                  <a href="#" class="footer__nav-item-title">Покупателям</a>
+                  <div class="footer__nav-subitems">
+                      <a class="footer__nav-subitem" href="#">Отзывы</a>
+                      <a class="footer__nav-subitem" href="#">Качество</a>
+                      <a class="footer__nav-subitem" href="#">Доставка</a>
+                      <a class="footer__nav-subitem" href="#">Оплата</a>
+                  </div>
+              </div>
+              <div class="footer__nav-item">
+                  <a href="#" class="footer__nav-item-title">Партнерам</a>
+                  <div class="footer__nav-subitems">
+                      <a class="footer__nav-subitem" href="#">Услуги</a>
+                      <a class="footer__nav-subitem" href="#">Франшиза</a>
+                  </div>
               </div>
           </div>
-          <div class="footer__nav-item">
-              <a href="#" class="footer__nav-item-title">Партнерам</a>
-              <div class="footer__nav-subitems">
-                  <a class="footer__nav-subitem" href="#">Услуги</a>
-                  <a class="footer__nav-subitem" href="#">Франшиза</a>
-              </div>
+          <div class="footer__dev">
+              <div class="footer__dev-copyright">Разработка сайта</div>
+              <a href="https://studio-aw.ru/" target="_blank" class="footer__dev-info">Digital студия A&W</a>
           </div>
-      </div>
-      <div class="footer__dev">
-          <div class="footer__dev-copyright">Разработка сайта</div>
-          <a href="https://studio-aw.ru/" target="_blank" class="footer__dev-info">Digital студия A&W</a>
       </div>
       <div class="footer__bottom">
 
@@ -84,11 +86,23 @@
 @use '@/assets/styles/helpers/functions';
 @use '@/assets/styles/helpers/mixins';
 
+.footer__top {
+    @include media.md-up {
+        display: grid;
+        grid-template-columns: 1fr auto;
+        grid-template-areas:
+            "nav contacts"
+            "nav dev";
+        grid-column-gap: 77px;
+    }
+}
+
 .footer__contacts-block {
     display: flex;
     flex-direction: column;
     gap: 40px;
     margin-bottom: 60px;
+    grid-area: contacts;
 
     @include media.md-up {
         margin-bottom: 40px;
@@ -99,7 +113,7 @@
 .footer__download-apps {
     display: grid;
     grid-gap: 15px;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(2, auto);
     margin-right: auto;
 }
 
@@ -137,33 +151,6 @@
 .footer__download-app-title {
     font-size: functions.rem(18);
     line-height: 1;
-}
-
-.footer__dev {
-    display: flex;
-    gap: 5px;
-    align-items: baseline;
-    flex-wrap: wrap;
-
-    &:hover {
-        &::before {
-            transition: border 0.5s;
-            border: 1px solid #fff;
-        }
-    }
-
-    &::before {
-        content: "";
-        position: relative;
-        top: 1px;
-        display: inline-block;
-        width: 10px;
-        height: 10px;
-        margin-right: 8px;
-        transition: border 0.5s;
-        border: 1px solid transparent;
-        background: var(--c-aw-color);
-    }
 }
 
 .footer__contacts {
@@ -267,12 +254,13 @@
     flex-wrap: wrap;
     column-gap: 15px;
     row-gap: 60px;
+    grid-area: nav;
 
     @include media.sm-down {
         margin-bottom: 60px;
     }
 
-    @include media.md-up {
+    @include media.lg-up {
         gap: 40px;
     }
 }
@@ -296,7 +284,11 @@
     display: flex;
     flex-direction: column;
     gap: 10px;
-    min-width: 160px;
+    min-width: 140px;
+
+    @include media.lg-up {
+        min-width: 130px;
+    }
 
     * {
         padding-block: 5px;
@@ -324,16 +316,49 @@
     }
 }
 
+.footer__dev {
+    display: flex;
+    gap: 5px;
+    align-items: baseline;
+    flex-wrap: wrap;
+    grid-area: dev;
+
+    &:hover {
+        &::before {
+            transition: border 0.5s;
+            border: 1px solid #fff;
+        }
+    }
+
+    &::before {
+        content: "";
+        position: relative;
+        top: 1px;
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        margin-right: 8px;
+        transition: border 0.5s;
+        border: 1px solid transparent;
+        background: var(--c-aw-color);
+    }
+}
+
 .footer__dev-copyright {
-    color: var(--c-grey-00);
-    font-weight: 300;
+    color: var(--c-grey80);
+    font-weight: 400;
     font-size: functions.rem(12);
 }
 
 .footer__dev-info {
     text-decoration: none;
-    color: var(--c-grey-00);
-    font-weight: 300;
+    color: var(--c-grey80);
+    font-weight: 400;
     font-size: functions.rem(14);
+    transition: all .2s linear;
+
+    &:hover {
+        color: var(--c-grey100);
+    }
 }
 </style>
