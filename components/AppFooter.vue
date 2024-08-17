3eeb3f967ff15c5a23c0ae526dcad5d243304f01
Juliya Snegova <juliya_snegova@studio-aw.ru>
2024-08-17 23:57:55 +0300

    add: add BaseIcon component add: add footer elements

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,18 +4,59 @@
 <template>
   <footer class="footer">
       <div class="footer__contacts-block">
-          <div class="footer__apps"></div>
+          <div class="footer__download-apps">
+              <div class="footer__download-app">
+                  <BaseIcon name="playmarket"/>
+                  <span class="footer__download-app-text">Скачать из</span>
+                  <span class="footer__download-app-title">Google Play</span>
+              </div>
+              <div class="footer__download-app">
+                  <BaseIcon name="appstore"/>
+                  <span class="footer__download-app-text">Загрузите в</span>
+                  <span class="footer__download-app-title">App Store</span>
+              </div>
+              <div class="footer__download-app">
+                  <BaseIcon name="huawei"/>
+                  <span class="footer__download-app-text">Откройте в</span>
+                  <span class="footer__download-app-title">AppGallery</span>
+              </div>
+          </div>
           <div class="footer__contacts"></div>
           <div class="footer__socials"></div>
       </div>
       <div class="footer__nav">
-
-      </div>
-      <div class="">
-          <div class="footer__dev">
-              <div class="footer__dev-copyright">Разработка сайта</div>
-              <a href="https://studio-aw.ru/" target="_blank" class="footer__dev-info">Digital студия A&W</a>
+          <div class="footer__nav-item">
+              <a href="" class="footer__nav-item-title">Рустерс</a>
+              <div class="footer__nav-subitems">
+                  <a class="footer__nav-subitem" href="">О Нас</a>
+                  <a class="footer__nav-subitem" href="">Новости</a>
+                  <a class="footer__nav-subitem" href="">Вакансии</a>
+                  <a class="footer__nav-subitem" href="">Контакты</a>
+              </div>
           </div>
+          <div class="footer__nav-item">
+              <a href="" class="footer__nav-item-title">Покупателям</a>
+              <div class="footer__nav-subitems">
+                  <a class="footer__nav-subitem" href="">Отзывы</a>
+                  <a class="footer__nav-subitem" href="">Качество</a>
+                  <a class="footer__nav-subitem" href="">Доставка</a>
+                  <a class="footer__nav-subitem" href="">Оплата</a>
+              </div>
+          </div>
+          <div class="footer__nav-item">
+              <a href="" class="footer__nav-item-title">Партнерам</a>
+              <div class="footer__nav-subitems">
+                  <a class="footer__nav-subitem" href="">Услуги</a>
+                  <a class="footer__nav-subitem" href="">Франшиза</a>
+              </div>
+          </div>
+      </div>
+      <div class="footer__dev">
+          <div class="footer__dev-copyright">Разработка сайта</div>
+          <a href="https://studio-aw.ru/" target="_blank" class="footer__dev-info">Digital студия A&W</a>
+      </div>
+      <div class="footer__bottom">
+
       </div>
   </footer>
 </template>
@@ -23,6 +64,55 @@
 <style scoped lang="scss">
 @use '@/assets/styles/helpers/media';
 @use '@/assets/styles/helpers/functions';
+
+.footer__contacts-block {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+}
+
+.footer__download-apps {
+    display: grid;
+    grid-gap: 15px;
+    grid-template-columns: repeat(2, 1fr);
+    margin-right: auto;
+}
+
+.footer__download-app {
+    display: inline-grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: repeat(2, auto);
+    align-items: center;
+    column-gap: 8px;
+    border: 1px solid var(--c-grey70);
+    padding: 7px 10px;
+    border-radius: var(--b-radius-md);
+    color: var(--c-grey70);
+
+    i {
+        font-size: functions.rem(25);
+        grid-row: 1/-1;
+
+        &:before {
+            margin: unset;
+        }
+    }
+
+    span {
+        grid-column: 2;
+    }
+}
+
+.footer__download-app-text {
+    font-size: functions.rem(8);
+    line-height: 1;
+    text-transform: uppercase;
+}
+
+.footer__download-app-title {
+    font-size: functions.rem(18);
+    line-height: 1;
+}
 
 .footer__dev {
     display: flex;
