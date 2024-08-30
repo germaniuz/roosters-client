4532e64ac94a9ba005530db59713f0a971850977
Juliya Snegova <juliya_snegova@studio-aw.ru>
2024-08-30 17:53:32 +0300

    * add: add news array * add: add news test images

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -1,12 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { NewsItem } from '~/types/news';
+
+type Props = {
+    newsItem: NewsItem;
+};
+
+defineProps<Props>();
+</script>
 
 <template>
     <div class="news-card">
         <div class="news-card__image"></div>
-        <div class="news-card__title"></div>
-        <div class="news-card__description"></div>
-        <div class="news-card__button"></div>
-        <BaseButton modifiers=""/>
+        <div class="news-card__title">{{ newsItem.title }}</div>
+        <div class="news-card__description">{{ newsItem.description }}</div>
+        <BaseButton modifiers="" />
     </div>
 </template>
 
---
+++ types/news.ts
@@ -0,0 +1,8 @@
+export type NewsItem = {
+    image: string;
+    title: string;
+    description: string;
+    badge: string;
+    link: string;
+    date: string;
+};
