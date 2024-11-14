694c2c390d21656dcf2c308a7449e3752399445f
Juliya Snegova <juliya_snegova@studio-aw.ru>
2024-11-14 16:50:38 +0300

    * refactor: refactor headings * refactor: add section style

--- components/PopularItems.vue
+++ components/PopularItems.vue
@@ -1,39 +1,49 @@
 <script setup lang="ts">
-
 import type { PopularItem } from '~/types/PopularItem';
 
-const popularItems: Array<PopularItem> = [{
-    title: 'Пышная Дружная семейка',
-    price: 1200,
-    img: '/images/test-pizza.webp',
-},{
-    title: 'Пышная',
-    price: 1200,
-    img: '/images/test-pizza.webp',
-},{
-    title: 'Пышная Дружная семейка',
-    price: 1200,
-    img: '/images/test-pizza.webp',
-},{
-    title: 'Пышная Дружная семейка',
-    price: 1200,
-    img: '/images/test-pizza.webp',
-},{
-    title: 'Пышная Дружная семейка',
-    price: 1200,
-    img: '/images/test-pizza.webp',
-}]
+const popularItems: Array<PopularItem> = [
+    {
+        title: 'Пышная Дружная семейка',
+        price: 1200,
+        img: '/images/test-pizza.webp',
+    },
+    {
+        title: 'Пышная',
+        price: 1200,
+        img: '/images/test-pizza.webp',
+    },
+    {
+        title: 'Пышная Дружная семейка',
+        price: 1200,
+        img: '/images/test-pizza.webp',
+    },
+    {
+        title: 'Пышная Дружная семейка',
+        price: 1200,
+        img: '/images/test-pizza.webp',
+    },
+    {
+        title: 'Пышная Дружная семейка',
+        price: 1200,
+        img: '/images/test-pizza.webp',
+    },
+];
 </script>
 
 <template>
-    <div class="popular-items">
-        <PopularItemCard v-for="popularItem in popularItems" :item="popularItem"/>
+    <div class="popular-items section">
+        <h2 class="h2 h2--icon"><BaseIcon name="sets" /> Часто заказывают</h2>
+        <div class="popular-items__grid">
+            <PopularItemCard v-for="popularItem in popularItems" :item="popularItem" />
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-
 .popular-items {
+}
+
+.popular-items__grid {
     display: flex;
     flex-direction: row;
     overflow: hidden;
