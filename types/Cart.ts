1f9b0ab0484d276aa13c63a0534e1f3255a7d4c2
Juliya Snegova <juliya_snegova@studio-aw.ru>
2025-02-07 13:04:46 +0300

    add CartProduct type

--- types/Cart.ts
+++ types/Cart.ts
@@ -1,3 +1,15 @@
+import type { Product } from '~/types/Product';
+
+export type CartProduct = {
+    quantity: number;
+    preset?: string;
+    product: {
+        id: number;
+        product_id: number;
+        product: Product;
+    };
+};
+
 export type DeliveryTimeOption = {
     id: number;
     title: string;
