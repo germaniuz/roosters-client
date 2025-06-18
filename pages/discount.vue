15aa79a279bf3ac0906e37badb2d49dec03e79af
German <web@studio-aw.ru>
2025-06-18 14:39:08 +0300

    fix products limit

---
+++ pages/discount.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container discount-page">
+        <h1 class="h1">Акции</h1>
+        <div class="discount-page__content">
+            <p>Здесь будут отображаться актуальные акции и предложения.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/helpers/functions';
+
+.discount-page {
+    padding-block: 20px 40px;
+}
+
+.discount-page__content {
+    margin-top: 30px;
+    color: var(--c-grey80);
+}
+</style>
