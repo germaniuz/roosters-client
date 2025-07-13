00c84ac6c831d5fd1d2451dcc4286422be9d55e0
German <germanius@list.ru>
2025-07-13 14:30:24 +0300

    feat(cart): add promocode discount support

--- types/Promocode.ts
+++ types/Promocode.ts
@@ -1,3 +1,11 @@
+import * as v from 'valibot';
+
+export const PromocodeSchema = v.object({
+    code: v.pipe(v.string(), v.nonEmpty('Введите промокод')),
+});
+
+export type PromocodeFields = v.InferOutput<typeof PromocodeSchema>;
+
 export type Promocode = {
     code: string;
 };
